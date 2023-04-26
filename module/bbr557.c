54837171e366bdd966d4ad617553124a5a85d7fc
Wangstonn <wangston@umich.edu>
2023-04-26 18:14:29 -0400

    fix 4

--- module/bbr557.c
+++ module/bbr557.c
@@ -360,13 +360,15 @@ static u32 bbr_target_cwnd(struct sock *sk, u32 bw, int gain)
 
 	if ((w > bbr->prev_w && w - bbr->prev_w > (bbr->prev_w >> BWRTT_TOL_B))||(w < bbr->prev_w  && bbr->prev_w - w > (bbr->prev_w >> BWRTT_TOL_B)))
 	{ //if change exceeds the tolerance decrease cycle length
-		//u16 tmp = (bbr->cycle_len - 1);
+		u16 tmp = (bbr->cycle_len - 1);
 		//bbr->cycle_len = (tmp < MIN_CYCLE_LEN)? MIN_CYCLE_LEN : tmp;
+		bbr->cycle_len = 8;
 	}
 	else
 	{
 		u16 tmp = (bbr->cycle_len + 1);
-		bbr->cycle_len = (tmp > MAX_CYCLE_LEN)? MAX_CYCLE_LEN : tmp;
+		//bbr->cycle_len = (tmp > MAX_CYCLE_LEN)? MAX_CYCLE_LEN : tmp;
+		bbr->cycle_len = 8;
 	}
 
 	bbr->prev_w = w;
