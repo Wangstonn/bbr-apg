64aa8f91aa4e99392666c8f7d978b18124fb9aab
Wangstonn <wangston@umich.edu>
2023-04-26 17:27:52 -0400

    Fixed cycle_len update

--- module/bbr557.c
+++ module/bbr557.c
@@ -360,14 +360,13 @@ static u32 bbr_target_cwnd(struct sock *sk, u32 bw, int gain)
 
 	if ((w > bbr->prev_w && w - bbr->prev_w > bbr->prev_w >> BWRTT_TOL_B)||(w < bbr->prev_w  && bbr->prev_w - w > bbr->prev_w >> BWRTT_TOL_B))
 	{
-		u16 tmp = (bbr->cycle_len + 1);
-		bbr->cycle_len = (tmp > MAX_CYCLE_LEN)? MAX_CYCLE_LEN : tmp;
-
+		u16 tmp = (bbr->cycle_len - 1);
+		bbr->cycle_len = (tmp < MIN_CYCLE_LEN)? MIN_CYCLE_LEN : tmp;
 	}
 	else
 	{
-		u16 tmp = (bbr->cycle_len - 1);
-		bbr->cycle_len = (tmp < MIN_CYCLE_LEN)? MIN_CYCLE_LEN : tmp;
+		u16 tmp = (bbr->cycle_len + 1);
+		bbr->cycle_len = (tmp > MAX_CYCLE_LEN)? MAX_CYCLE_LEN : tmp;
 	}
 
 	bbr->prev_w = w;
