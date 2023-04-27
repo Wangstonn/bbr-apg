ed8c0df3441cf942fa73cde2c974027e88a5fa1f
Wangstonn <Wangston@umich.edu>
2023-04-27 03:48:44 -0400

    decrease tol

--- mahimahi/run_experiments.sh
+++ mahimahi/run_experiments.sh
@@ -7,13 +7,13 @@ set -e # Stop if any error occurs.
 ./initialize_congestion_control.sh
 
 # Generate all the needed data.
-#./run_figure8_experiment.sh $@
+##./run_figure8_experiment.sh $@
 ./557_run_experiment1.sh $@
 ./557_run_experiment2.sh $@
-./557_run_experiment3.sh $@
-./run_experiment4.sh $@
-./557_run_experiment5.sh $@
-#./557_run_experiment6.sh $@
-./557_run_experiment7.sh $@
-# Plot the results.
-#./bbr_plot.py
+#./557_run_experiment3.sh $@
+#./run_experiment4.sh $@
+#./557_run_experiment5.sh $@
+##./557_run_experiment6.sh $@
+#./557_run_experiment7.sh $@
+## Plot the results.
+##./bbr_plot.py
